fa23fae196a1cc76bdb7c6411501e9999b14c721
m17saitou <m17saitou@gmail.com>
2019-11-10 18:52:19 +0900

    Add:メンバ関数をいくつか作成

--- seatChange.cpp
+++ seatChange.cpp
@@ -1,1 +1,26 @@
 #include "seatChange.hpp"
+
+SeatChange::SeatChange(int w , int h){
+    SeatChange::width = w;
+    SeatChange::height = h;
+    seatArray.resize(w*h+1);
+    for(int y=0;y<=SeatChange::height;y++){
+        for(int x=0;x<=SeatChange::width;x++){
+            seatArray[y*SeatChange::width + x] = 0;
+        }
+    }
+}
+void SeatChange::setSeatData(std::vector <int> seatArr){
+    for(int y=1;y<=SeatChange::height;y++){
+        for(int x=1;x<=SeatChange::width;x++){
+            seatArray[y*SeatChange::width + x] = seatArr[y*SeatChange::width + x];
+        }
+    }
+}
+std::vector<int> SeatChange::getSeatData(){
+    return seatArray;
+}
+int SeatChange::getSeatID(int x,int y){
+    int id = seatArray[y*SeatChange::width + x];
+    return id;
+}
--- seatChange.hpp
+++ seatChange.hpp
@@ -12,8 +12,8 @@ public :
     static int height;
     SeatChange( int w, int h);//コンストラクタ 高さと幅を与えて初期化
     void setSeatData(std::vector <int> seatArr[]);//Vectorを与えて席順配列を作る．
-    std::vector <int> getSeatData();//席順配列を返す
-    int getSeatNum(int x, int y);//座標からその席の出席番号を返す
+    std::vector<int> getSeatData();//席順配列を返す
+    int getSeatID(int x, int y);//座標からその席の出席番号を返す
     void newSeat(std::vector <int[3]> defSeat);//新しい席順を生成する vectorで絶対的に固定できる．フォーマットは{x,y,出席番号}
     void fromCsvSeat(std::string filename);
     ~SeatChange();//デストラクタ (一応)
